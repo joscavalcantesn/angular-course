31731b8961256f34efa07a4be213634d59b6e328
joscavalcantesn <joscavalcantesn@gmail.com>
2025-10-18 14:47:43 -0300

    feat(app): add user selection with signals and show tasks or fallback message

--- easy-tasks/src/app/app.ts
+++ easy-tasks/src/app/app.ts
@@ -1,22 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { Tasks } from './tasks/tasks';
+import { DUMMY_USERS } from './user/mock/dummy-users';
 import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, User],
+  imports: [RouterOutlet, Header, User, Tasks],
   template: `
     <app-header></app-header>
+
     <main>
       <ul id="users">
+        @for (user of users(); track user.id) {
         <li>
-          <app-user />
+          <app-user [user]="user" (select)="onSelectUser($event)" />
         </li>
+        }
       </ul>
+
+      @if(selectedUserId()) {
+      <app-tasks [username]="selectedUserName()" />
+      } @else {
+      <h2 id="fallback">Select a user to see their tasks!</h2>
+      }
     </main>
+
     <router-outlet />
   `,
   styleUrl: 'app.scss',
 })
-export class App {}
+export class App {
+  users = signal(DUMMY_USERS);
+  selectedUserId = signal<string | null>(null);
+
+  selectedUserName = computed(
+    () => this.users().find((user) => user.id === this.selectedUserId()!)?.name ?? ''
+  );
+
+  onSelectUser(userId: string) {
+    this.selectedUserId.set(userId);
+  }
+}
